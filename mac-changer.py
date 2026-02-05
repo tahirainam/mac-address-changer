6d65d7f370efca716d1a20ccf5c0c10c1b021890
tahirainam <cattblackwhite@gmail.com>
2026-02-05 02:39:37 -0800

    added variables for interface and mac address

--- mac-changer.py
+++ mac-changer.py
@@ -1,5 +1,10 @@
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True) #use eth0 for wired or virtual adapter
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:77", shell=True) #use eth0 for wired or virtual adapter
-subprocess.call("ifconfig wlan0 up", shell=True) #use eth0 for wired or virtual adapter
+interface = input("interface: ") #use eth0 for wired or virtual adapter and wlan0 for wireless
+new_mac = input("new MAC address: ")
+
+print("[+] changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True) 
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True) 
+subprocess.call("ifconfig " + interface + " up", shell=True) 
